18e65042e17131d4cf32e6e8a548b6519fdeb0b4
Akhilesh Babu <ab106@snu.edu.in>
2024-04-19 11:50:51 +0530

    new file added

---
+++ hello-world-file-destructure/first.tf
@@ -0,0 +1,4 @@
+//Running Terraform plan on particular directory will run all the .tf files in that perticular directory.
+output firstoutputblock{
+    value = "This is first output block"
+}
---
+++ hello-world-file-destructure/second.tf
@@ -0,0 +1,5 @@
+//Running Terraform plan on particular directory will run all the .tf files in that perticular directory.
+
+output "secondoutputblock"{
+    value= "This is second output block."
+}
